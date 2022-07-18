081ea432042a222d9528a07d3dabeb9505c6cb4b
NguyenManhHien12797 <nguyenhien81f@gmail.com>
2022-07-18 15:40:00 +0700

    add parameter

--- src/MiniTest.java
+++ src/MiniTest.java
@@ -28,24 +28,24 @@ public class MiniTest {
             choice= scanner.nextInt();
             switch (choice){
                 case 1:
-                    System.out.println("Lương trung bình của nhân viên là: "+getAvgSalary());
-                    System.out.println("Danh sách nhân viên có lương nhỏ hơn lương trung bình:\n"+getListEmployeeFullTime());
+                    System.out.println("Lương trung bình của nhân viên là: "+getAvgSalary(employee));
+                    System.out.println("Danh sách nhân viên có lương nhỏ hơn lương trung bình:\n"+getListEmployeeFullTime(employee,getAvgSalary(employee)));
                     System.out.println("----------------------------------------------------");
                     break;
                 case 2:
-                    System.out.println("Lương phải trả cho nhân viên partTime là: "+totalSalaryPartTime());
+                    System.out.println("Lương phải trả cho nhân viên partTime là: "+totalSalaryPartTime(employee));
                     System.out.println("------------------------------------------------------");
                     break;
                 case 3:
-                    System.out.println(sortEmployFullTime());
+                    System.out.println(sortEmployFullTime(employee));
                     System.out.println("------------------------------------------------------");
                     break;
                 case 4:
-                    System.out.println(addEmployeeFullTime());
+                    System.out.println(addEmployeeFullTime(employee));
                     System.out.println("------------------------------------------------------");
                     break;
                 case 5:
-                    System.out.println(addEmployeePartTime());
+                    System.out.println(addEmployeePartTime(employee));
                     System.out.println("------------------------------------------------------");
                     break;
                 case 6:
@@ -69,19 +69,19 @@ public class MiniTest {
                 "6. Thoát");
         System.out.println("-----------------------------------------------------------");
     }
-    public static double getAvgSalary(){
+    public static double getAvgSalary(ArrayList<Employee> arr){
         double sum=0;
-        for(Employee e: employee){
+        for(Employee e: arr){
             sum+= e.getNetSalary();
         }
-        return (sum/employee.size());
+        return (sum/arr.size());
     }
         // Liệt kê sô nhân viên có lương nhỏ hơn lương trung bình
-    public static ArrayList<Employee> getListEmployeeFullTime(){
+    public static ArrayList<Employee> getListEmployeeFullTime(ArrayList<Employee> arr, double avg){
         ArrayList<Employee> emp= new ArrayList<>();
-        for(Employee e: employee){
+        for(Employee e: arr){
             if(e instanceof EmployeeFullTime){
-                if(e.getNetSalary()<getAvgSalary()){
+                if(e.getNetSalary()<avg){
                     emp.add(e);
                 }
             }
@@ -89,9 +89,9 @@ public class MiniTest {
         return emp;
     }
     // Tính lương trả cho nv PartTime
-    public static double totalSalaryPartTime(){
+    public static double totalSalaryPartTime(ArrayList<Employee> arr){
         double total=0;
-        for(Employee e: employee){
+        for(Employee e: arr){
             if(e instanceof EmployeePartTime){
                 total+= e.getNetSalary();
             }
@@ -99,9 +99,9 @@ public class MiniTest {
         return total;
     }
     //Sắp xếp nhân viên FullTime có lương tăng dần
-    public static ArrayList<Employee> sortEmployFullTime(){
+    public static ArrayList<Employee> sortEmployFullTime(ArrayList<Employee>arr1){
         ArrayList<Employee> newarr= new ArrayList<>();
-        for(Employee arr: employee){
+        for(Employee arr: arr1){
             if( arr instanceof EmployeeFullTime){
               newarr.add(arr);
                 newarr.sort((((o1, o2) ->
@@ -112,7 +112,7 @@ public class MiniTest {
         return newarr;
     }
     //Thêm nhân viên
-    public static ArrayList<Employee> addEmployeeFullTime(){
+    public static ArrayList<Employee> addEmployeeFullTime(ArrayList<Employee> arr){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number Employee: ");
         int number = scanner.nextInt();
@@ -139,12 +139,12 @@ public class MiniTest {
             double fixedSalary= scanner.nextDouble();
             EmployeeFullTime x= new EmployeeFullTime(code,
                     name,age,phone,email, bonus, fine,fixedSalary);
-            employee.add(x);
+            arr.add(x);
         }
 
-        return employee;
+        return arr;
     }
-    public static ArrayList<Employee> addEmployeePartTime(){
+    public static ArrayList<Employee> addEmployeePartTime(ArrayList<Employee> arr){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number Employee: ");
         int number = scanner.nextInt();
@@ -167,9 +167,9 @@ public class MiniTest {
             double workingHours= scanner.nextDouble();
             EmployeePartTime x= new EmployeePartTime(code,
                     name,age,phone,email,workingHours);
-            employee.add(x);
+            arr.add(x);
         }
 
-        return employee;
+        return arr;
     }
 }
